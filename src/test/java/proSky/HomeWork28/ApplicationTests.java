3881ae370b63962222d250a7204d4b1a873a04a4
Павел <Pavel-grudev@yandex.ru>
2023-05-14 11:35:10 +0300

    Master check1

---
+++ src/test/java/proSky/HomeWork28/ApplicationTests.java
@@ -0,0 +1,13 @@
+package proSky.HomeWork28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
